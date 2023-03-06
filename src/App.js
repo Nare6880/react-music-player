414525970d924728e760642d6afff9b586439eaf
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2023-03-05 20:11:37 -0500

    ModifyLayout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Playlists from "./components/Playlists";
+import Songs from "./components/Songs";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsUpDownLeftRight } from "@fortawesome/free-solid-svg-icons";
@@ -11,6 +12,8 @@ const defaultComponentList = [
 	{ type: "Playlists", id: "1" },
 	{ type: "Playlists", id: "2" },
 	{ type: "Playlists", id: "3" },
+	{ type: "Songs", id: "4" },
+	{ type: "Playlists", id: "5" },
 ];
 function App() {
 	const [components, updateComponents] = useState(defaultComponentList);
@@ -24,43 +27,63 @@ function App() {
 		updateComponents(items);
 	}
 	return (
-		<DragDropContext onDragEnd={handleOnDragEnd}>
-			<Droppable droppableId="components">
-				{(provided) => (
-					<div
-						className="Grid"
-						{...provided.droppableProps}
-						ref={provided.innerRef}
-					>
-						{components.map(({ type, id }, index) => {
-							return (
-								<Draggable key={id} draggableId={id} index={index}>
-									{(provided) => (
-										<div ref={provided.innerRef} {...provided.draggableProps}>
-											<div className="component-header">
-												<h2 className="component-header-text">{type}</h2>
-												<div
-													className="component-header-decoration"
-													{...provided.dragHandleProps}
-												>
-													<FontAwesomeIcon
-														icon={faArrowsUpDownLeftRight}
-														size="2x"
-													/>
+		<>
+			<DragDropContext onDragEnd={handleOnDragEnd}>
+				<Droppable droppableId="components">
+					{(provided, snapshot) => (
+						<ul
+							className="component-2col"
+							{...provided.droppableProps}
+							style={{
+								backgroundColor: snapshot.isDraggingOver ? "blue" : "grey",
+							}}
+							ref={provided.innerRef}
+						>
+							{components.map(({ type, id }, index) => {
+								return (
+									<Draggable key={id} draggableId={id} index={index}>
+										{(provided) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												className="component-container"
+											>
+												<div className="component-header">
+													<h2 className="component-header-text">
+														{type} {id}
+													</h2>
+													<div
+														className="component-header-decoration"
+														{...provided.dragHandleProps}
+													>
+														<FontAwesomeIcon
+															icon={faArrowsUpDownLeftRight}
+															size="2x"
+														/>
+													</div>
 												</div>
+												{getComponent({ type })}
 											</div>
-											<Playlists></Playlists>
-										</div>
-									)}
-								</Draggable>
-							);
-						})}
-						{provided.placeholder}
-					</div>
-				)}
-			</Droppable>
-		</DragDropContext>
+										)}
+									</Draggable>
+								);
+							})}
+							{provided.placeholder}
+						</ul>
+					)}
+				</Droppable>
+			</DragDropContext>
+		</>
 	);
+	function getComponent(object) {
+		switch (object.type) {
+			case "Playlists":
+				return <Playlists></Playlists>;
+			case "Songs":
+				return <Songs></Songs>;
+				break;
+		}
+	}
 }
 
 export default App;
---
+++ src/components/Songs.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import data from "./Data/songData.json";
+
+function Songs() {
+	console.log(data);
+	const [songs, updateSongs] = useState(data);
+	function handleOnDragEnd(result) {
+		if (!result.destination) return;
+
+		const items = Array.from(songs);
+		const [reorderedItem] = items.splice(result.source.index, 1);
+		items.splice(result.destination.index, 0, reorderedItem);
+
+		updateSongs(items);
+	}
+	return (
+		<div>
+			<DragDropContext onDragEnd={handleOnDragEnd}>
+				<Droppable droppableId="songs">
+					{(provided) => (
+						<ul
+							className=""
+							{...provided.droppableProps}
+							ref={provided.innerRef}
+						>
+							{songs.map(({ title, artist, album, runtime }, index) => {
+								return (
+									<Draggable
+										key={index}
+										draggableId={index.toString()}
+										index={index}
+									>
+										{(provided) => (
+											<li
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												<div></div>
+												<p>{title}</p>
+												<p>{artist}</p>
+												<p>{album}</p>
+												<p>{runtime}</p>
+											</li>
+										)}
+									</Draggable>
+								);
+							})}
+							{provided.placeholder}
+						</ul>
+					)}
+				</Droppable>
+			</DragDropContext>
+		</div>
+	);
+}
+
+export default Songs;
