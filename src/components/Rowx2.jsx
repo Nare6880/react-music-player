6006348ab599c3d02434082fb87cc0251090ffd3
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2023-03-08 23:30:07 -0500

    CleanUpApp.js add ScriptHook

---
+++ src/components/Rowx2.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Playlists from "./Playlists";
+import Songs from "./Songs";
+import useScript from "../hooks/useScript";
+function Rowx2({ Customize, Row }) {
+	return (
+		<div className="flex-2">
+			{Row.map(({ type, id }, index) => {
+				return (
+					<>
+						<div className="component-container">
+							<div className="component-header">
+								<h2 className="component-header-text">
+									{type} {id}
+								</h2>
+								<div className="component-header-decoration"></div>
+							</div>
+							{getComponent({ type })}
+						</div>
+						{index === 0 && (
+							<div
+								className="division"
+								onClick={() =>
+									function () {
+										console.log("clicked");
+									}
+								}
+							></div>
+						)}
+					</>
+				);
+			})}
+		</div>
+	);
+	function getComponent(object) {
+		switch (object.type) {
+			case "Playlists":
+				return <Playlists dragDisabled={Customize}></Playlists>;
+			case "Songs":
+				return <Songs dragDisabled={Customize}></Songs>;
+				break;
+		}
+	}
+}
+
+export default Rowx2;
